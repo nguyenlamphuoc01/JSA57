9f233a35f614688a90288fd3b77448a8238c412f
nguyenlamphuoc01 <nguyenlamphuoc01@gmail.com>
2025-08-27 20:38:35 +0700

    b5

---
+++ B4/js/object.js
@@ -0,0 +1,45 @@
+// khai bao-------------------------------
+const obj = {};
+console.log(obj);
+
+//them thuoc tinh moi---------------------
+obj.a = 10;
+console.log(obj);
+
+// tao loai cho object--------------------
+class Animal {
+  constructor(name) {
+    this.$name = name;
+    this.$speak = "abc";
+  }
+}
+
+// tao obj
+const obj1 = new Animal("mno");
+console.log(obj1);
+
+// duyet object (for in) ------------------
+for (let key in obj1) {
+  console.log(key, obj1[key]);
+}
+
+// toan tu in -> return bool ---------------------------
+console.log("a" in obj || "Khong co"); // kiem tra key a co ton tai trong obj
+
+// ham hasOwnProperty -> return bool ------------------
+console.log(obj1.hasOwnProperty("$gender") ? "co" : "khong"); // false (khong)
+
+// xoa key -------------------
+const obj2 = { ...obj1, gender: 0 };
+console.log(obj2);
+
+// toan tu rest --------------
+const { $name: animalName, ...otherInfo } = obj2;
+console.log(animalName);
+console.log(otherInfo);
+
+//--------------------------
+// KIEM TRA LOAI
+console.log(typeof []); // object -x array
+console.log(Array.isArray([])); // true
+console.log(typeof {}); // object
