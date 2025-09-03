6dbbc295d5d76e82c7c0c4accd5b91d2172b1ea0
nguyenlamphuoc01 <nguyenlamphuoc01@gmail.com>
2025-09-03 21:01:34 +0700

    b6

---
+++ BTVN/buoi4/js/script.js
@@ -0,0 +1,12 @@
+const students = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+  { id: 4, name: "Diana" },
+  { id: 5, name: "Ethan" },
+  { id: 6, name: "Fiona" },
+  { id: 7, name: "George" },
+  { id: 8, name: "Hannah" },
+  { id: 9, name: "Ivan" },
+  { id: 10, name: "Julia" }
+];
